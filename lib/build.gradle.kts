e256a8a08dca2fd72628d918713811e6a66e9758
simon <75259597+tzimom@users.noreply.github.com>
2026-01-19 19:18:47 +0100

    Initial commit.

---
+++ lib/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("java-library")
+}
+
+repositories {
+    mavenCentral()
+
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
---
+++ lib/src/main/java/net/tzimom/chainbreak/ChainBreakPlugin.java
@@ -0,0 +1,9 @@
+package net.tzimom.chainbreak;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChainBreakPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
